6fe271e363e5cb1c0dd0a252cad2b390f94e8a46
dityap <115879765+dityap@users.noreply.github.com>
2023-01-18 13:15:39 +0700

    Add files via upload

---
+++ UTS_Homepage/UTS School Homepage/tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#5E8B7E',
+            },
+            fontFamily: {
+                raleway: ['Inter', 'sans-serif'],
+            },
+        }
+    }
+}
